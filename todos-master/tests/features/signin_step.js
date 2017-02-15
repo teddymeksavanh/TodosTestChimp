fde4270560970e84c70558c5dd334451c449555e
tmeksava <teddy.meksavanh@arkena.com>
2017-02-15 23:58:37 +0100

    watch on loggin

--- todos-master/tests/features/signin_step.js
+++ todos-master/tests/features/signin_step.js
@@ -1,39 +1,38 @@
-import { jQuery } from 'jquery';
-
 module.exports = function () {
     'use strict';
 
     this.Given(/^I am on the homepage$/, function () {
         // Write code here that turns the phrase above into concrete actions
         browser.url("http://localhost:3000");
-        let _el = '.esgi-signin';
+        let _el = '.esgi-join';
         browser.waitForVisible(_el);
     });
 
     this.When(/^I click on the element "([^"]*)"$/, function (arg1) {
         // Write code here that turns the phrase above into concrete actions
-        browser.click('.esgi-signin');
+        browser.click(arg1);
+        let _el = '#at-field-email';
+        browser.waitForVisible(_el);
     });
 
-    this.Then(/^I should see the element "([^"]*)"$/, function (arg1) {
+    this.When(/^I set my email "([^"]*)"$/, function (arg1) {
         // Write code here that turns the phrase above into concrete actions
-        browser.waitForVisible('input[name="at-field-email"');
+        browser.setValue('input[name="at-field-email"]', arg1);
     });
 
-    this.When(/^I fill the element "([^"]*)" with "([^"]*)"$/, function (arg1, arg2) {
+    this.When(/^I set my password "([^"]*)"$/, function (arg1) {
         // Write code here that turns the phrase above into concrete actions
+        browser.setValue('input[name="at-field-password"]', arg1);
     });
 
-    this.When(/^I fill the element "([^"]*)" with "([^"]*)"$/, function (arg1, arg2) {
+    this.When(/^I valid my password"([^"]*)"$/, function (arg1) {
         // Write code here that turns the phrase above into concrete actions
-    });
-
-    this.When(/^I fill the element "([^"]*)" with "([^"]*)"$/, function (arg1, arg2) {
-        // Write code here that turns the phrase above into concrete actions
+        browser.setValue('input[name="at-field-password_again"]', arg1);
     });
 
     this.When(/^I submit "([^"]*)"$/, function (arg1) {
         // Write code here that turns the phrase above into concrete actions
+        browser.click(arg1);
     });
 
     this.Then(/^I should see the element "([^"]*)"$/, function (arg1) {
--- todos-master/tests/loggin.feature
+++ todos-master/tests/loggin.feature
@@ -1,14 +1,14 @@
-Feature: User signin
+@watch
+Feature: User loggin
 
   As a human
   I want to loggin
   So I can use the app
 
-  Scenario: Signin
+  Scenario: Loggin
     Given I am on the homepage
-    When I click on the element ".esgi-join"
+    When I click on the element ".esgi-login"
     When I set my email "test@gmail.com"
     When I set my password "123456"
-    When I valid my password"123456"
     When I click on the element ".esgi-submit"
     Then I should see the element ".js-user-menu"
